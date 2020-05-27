a5ab44939b1448a959232860351e8c2032feac5c
M-3-6 <63166735+M-3-6@users.noreply.github.com>
2020-05-27 22:58:30 +0530

    Update script.js

--- module-5-solution/js/script.js
+++ module-5-solution/js/script.js
@@ -60,6 +60,7 @@ var switchMenuToActive = function () {
   }
 };
 
+
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
 
@@ -84,7 +85,7 @@ showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
   function(categories){
-  buildAndShowHTML(categories);
+    buildAndShowHomeHTML (categories);
   }, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
@@ -103,10 +104,9 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-       var chosenCategoryShortName = chooseRandomCategory (categories).short_name;
+      var chosenCategoryShortName = chooseRandomCategory (categories).short_name;
       chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
-
-
+      
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName);
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
